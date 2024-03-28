044ba09e4dc25b56d92d3e3c31acfe34696044d6
HyunjinKim <koujie11@gmail.com>
2024-03-28 18:43:51 +0900

    style : common button add color #12

--- src/components/common/mypage/button.tsx
+++ src/components/common/mypage/button.tsx
@@ -3,7 +3,7 @@ export const EditButton = () => {
     alert('수정하기 버튼입니다.');
   };
   return (
-    <button onClick={handleOnClickEditBtn} className="p-4 border rounded-xl ">
+    <button onClick={handleOnClickEditBtn} className="p-4 border rounded-xl w-[150px]">
       수정하기
     </button>
   );
@@ -14,8 +14,12 @@ export const CancleButton = () => {
     alert('취소하기 버튼입니다.');
   };
   return (
-    <button onClick={handleOnClickCancleBtn} className="p-4 border rounded-xl">
+    <button onClick={handleOnClickCancleBtn} className="p-4 border rounded-xl w-[150px]  bg-rose-500 text-white">
       취소하기
     </button>
   );
+};
+
+export const GoToClassPost = () => {
+  return <button className="border rounded-xl p-4 w-[150px]">클래스 보러가기</button>;
 };
