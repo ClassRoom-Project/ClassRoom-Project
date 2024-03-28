faa16364fff3b8324e78e89365797e1f64821187
Jiwon <tj1tj2tj3@naver.com>
2024-03-28 15:55:25 +0900

    sytle: 예약 완료 페이지 틀잡기 #13

---
+++ src/app/reserve/[rid]/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const reservationCompletePage = ({ params }: { params: { rid: string } }) => {
+  const reservationId = decodeURIComponent(params.rid);
+  console.log(reservationId);
+
+  return (
+    <div className="w-[1600px] h-[800px]">
+      <h1 className="text-xl">예약 완료</h1>
+      <div className="w-full h-full bg-gray-200 p-6 flex flex-col justify-between items-center">
+        <h1 className="text-xl text-center">예약이 정상적으로 처리되었습니다..</h1>
+        <div className="flex flex-col w-1/3 gap-6">
+          <div className="flex w-full justify-between gap-4">
+            <span className="w-20 text-right">클래스명</span>
+            <span className="w-52 text-center">[요리] 쫀득쫀득 스모어쿠키</span>
+          </div>
+          <div className="flex w-full justify-between gap-4">
+            <span className="w-20 text-right">이용 일자</span>
+            <span className="w-52 text-center"> 2024-03-26 </span>
+          </div>
+          <div className="flex w-full justify-between gap-4">
+            <span className="w-20 text-right">이용 회차</span>
+            <span className="w-52 text-center"> 16:00 (1회차) </span>
+          </div>
+          <div className="flex w-full justify-between gap-4">
+            <span className="w-20 text-right">이용 인원</span>
+            <span className="w-52 text-center"> 1 명 </span>
+          </div>
+          <div className="flex w-full justify-between gap-4">
+            <span className="w-20 text-right">이용 금액</span>
+            <span className="w-52 text-center"> 50,000원 </span>
+          </div>
+        </div>
+        <div className="flex gap-32">
+          <button className="bg-white w-48 p-4 text-center self-end">메인페이지 바로가기</button>
+          <button className="bg-white w-48 p-4 text-center self-end">예약 상세 보기</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default reservationCompletePage;
