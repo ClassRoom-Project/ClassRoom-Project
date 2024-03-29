ee0eec5d4157c6c8de4b1407a915ff3d7a9c455f
ocean <twyeo9006@gmail.com>
2024-03-29 18:35:31 +0900

    fix:BorderLayout 패딩조정

--- src/components/common/BorderLayout.tsx
+++ src/components/common/BorderLayout.tsx
@@ -21,7 +21,7 @@ const Layout = ({ children }: PropsWithChildren) => {
       <div className="fixed top-0 bottom-0 min-h-screen right-0 w-[15px]bg-[#F0F6FF] z-40"></div>
 
       <main className="bg-[#F0F6FF] p-[15px] pl-[100px]">
-        <div className="h-full bg-white rounded-[20px] pl-[100px] p-[15px]">{children}</div>
+        <div className="h-full bg-white rounded-[20px] p-[5px]">{children}</div>
       </main>
 
       <footer className="fixed bottom-0 left-0 right-0 h-[15px]bg-[#F0F6FF] z-50"></footer>
