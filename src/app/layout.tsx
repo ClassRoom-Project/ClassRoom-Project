29454d413efad741e8a22dd0794f3b2de67cc221
limhyerin <rin091345@gmail.com>
2024-03-29 15:41:53 +0900

    merge: get code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import MainLayout from '@/components/main/MainLayout';
 
 import Link from 'next/link';
 import LoginPage from './login/page';
-import Layout from '@/components/common/layout';
+import Layout from '@/components/common/Layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
