bf1f7eb8cf2df37ecfa7f28eb3fa6c3a70dd4e41
HyunjinKim <koujie11@gmail.com>
2024-03-30 18:33:41 +0900

    feat :  마이페이지 들어왔을 때 user 정보 초기값 불러오기

--- src/components/mypage/EditProfile.tsx
+++ src/components/mypage/EditProfile.tsx
@@ -1,16 +1,39 @@
-import Image from 'next/image';
-import React, { useState } from 'react';
-import { CancleButton, EditButton } from '../common/mypage/buttons';
 import { UserType } from '@/types/user';
-import { useGetUserInfo } from '@/hooks/mypage/useGetUserInfo';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
 import BasicProfileImage from '../../../public/profile-image.png';
+import { CancleButton, EditButton } from '../common/mypage/buttons';
+import { getUserInfo } from '@/api/user-api';
+import { useQuery } from '@tanstack/react-query';
+import { userId } from '@/app/mypage/page';
 
 const EditProfile = () => {
-  const userInfo = useGetUserInfo();
-  // console.log('userInfo', userInfo);
-  const [nickname, setNickname] = useState(userInfo?.nickname);
-  const [email, setEmail] = useState(userInfo?.email);
-  const [password, setPassword] = useState(userInfo?.password);
+  const { data: userInfo, isPending }: { data: UserType | undefined; isPending: boolean } = useQuery({
+    queryKey: ['user', userId],
+    queryFn: () => getUserInfo()
+  });
+
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
+
+  useEffect(() => {
+    if (userInfo) {
+      setNickname(userInfo.nickname || '');
+      setEmail(userInfo.email || '');
+      setPassword(userInfo.password || '');
+      setCheckPassword(userInfo.password || '');
+    }
+  }, [userInfo]);
+
+  if (isPending) {
+    return <div> 로딩중 ... </div>;
+  }
+
+  if (!userInfo) {
+    return <div> 유저 정보가 없습니다.</div>;
+  }
 
   const handleOnChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
@@ -21,10 +44,12 @@ const EditProfile = () => {
   const handleOnChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
+  const handleOnChangeCheckPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckPassword(e.target.value);
+  };
 
-  if (!userInfo) {
-    return <div>로딩 중...</div>;
-  }
+  const handleOnClickEditProfileBtn = () => {};
+
   return (
     <div className="flex">
       <div className="flex flex-col items-center p-4 gap-4">
@@ -47,11 +72,13 @@ const EditProfile = () => {
           </div>
           <div className="m-4 p-4">
             <p>비밀번호 확인</p>
-            <input type="text" className="border p-1" />
+            <input type="text" className="border p-1" value={checkPassword} onChange={handleOnChangeCheckPassword} />
           </div>
         </div>
         <div className="m-4 p-4 flex gap-4">
-          <EditButton />
+          <button onClick={handleOnClickEditProfileBtn} className="p-4 border rounded-xl w-[150px]">
+            수정하기
+          </button>
           <CancleButton />
         </div>
       </div>
