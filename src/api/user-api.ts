c16c41344ef65f3961413fd3a05f77f7154c118d
HyunjinKim <koujie11@gmail.com>
2024-03-30 18:32:27 +0900

    feat : 마이페이지 - 프로필 수정 user 정보 불러오기 #14

--- src/api/user-api.ts
+++ src/api/user-api.ts
@@ -1,15 +1,34 @@
-import { userId } from '@/hooks/mypage/useGetUserInfo';
+import { PostgrestMaybeSingleResponse } from '@supabase/supabase-js';
 import { supabase } from './supabase/supabase';
 import { UserType } from '@/types/user';
+import { userId } from '@/app/mypage/page';
 
-export const getUser = async (): Promise<UserType> => {
-  const { data, error } = await supabase.from('user').select('*').eq('user_id', userId).single();
-  // console.log('data', data);
-  // console.log('data', typeof data);
+// User가 선생님인지 수강생인지 구분 : teacher 값 불러오기
+export const getUserRole = async ({ userId }: { userId: string }) => {
+  const { data: userRole, error }: PostgrestMaybeSingleResponse<UserType> = await supabase
+    .from('user')
+    .select('teacher')
+    .eq('user_id', userId)
+    .single();
 
   if (error) {
     console.error(error);
   }
-  // console.log(data);
-  return data;
+
+  return userRole;
+};
+
+// User 정보 불러오기
+export const getUserInfo = async () => {
+  const { data: userInfo, error }: PostgrestMaybeSingleResponse<UserType> = await supabase
+    .from('user')
+    .select('nickname, email, password, profile_image')
+    .eq('user_id', userId)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return userInfo;
 };
