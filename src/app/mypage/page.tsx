13da4541f26eb011d0a1fe520ee91bcd57b289fa
limhyerin <rin091345@gmail.com>
2024-03-31 19:13:35 +0900

    test: branch 변경 #17

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -5,7 +5,7 @@ import TeacherMyPageTab from '@/components/mypage/teacher/TeacherMyPageTab';
 // 일단 임시로 생성 userId : 로그인한 사람의 user_id => zustand로 전역관리
 // 실제 로그인한 사람의 user_id가 들어가야함!
 // export const userId = '423e4567-e89b-12d3-a456-426614174004'; // admin : true인 사람 (선생님)
-export const userId = '523e4567-e89b-12d3-a456-426614174005'; // admin : false인 사람 (수강생)
+export const userId = '223e4567-e89b-12d3-a456-426614174002'; // admin : false인 사람 (수강생)
 
 const MyPage = async () => {
   // 일단 teacher :  teacher = true / student = false 이라고 가정
@@ -15,7 +15,7 @@ const MyPage = async () => {
   return (
     <div>
       {/* teacher의 boolean 값에 따라 마이페이지 구분*/}
-      {isTeacher ? <TeacherMyPageTab /> : <StudentMyPageTab />}
+      {!isTeacher ? <TeacherMyPageTab /> : <StudentMyPageTab />}
     </div>
   );
 };
