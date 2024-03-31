71eb35eb7c92961089da89476331475837a6bc3e
ocean <twyeo9006@gmail.com>
2024-03-31 21:51:06 +0900

    feat : 각 카드에 데이터 집어넣고 각 class div에 넣기

--- src/api/supabase/fetchClassInfo.ts
+++ src/api/supabase/fetchClassInfo.ts
@@ -22,8 +22,8 @@ export const fetchClassInfo = async ({ classId }: { classId: string }) => {
 
 // 여러 클래스 정보를 불러오는 함수
 //따로 만든 이유 -> single이 단일 정보만 불러오기 때문
-export const fetchClassInfos = async ({ classId }: { classId: string }) => {
-  const { data: classInfos, error } = await supabase.from('class').select('*').eq('class_id', classId);
+export const fetchClassInfos = async () => {
+  const { data: classInfos, error } = await supabase.from('class').select('*');
 
   if (error) {
     console.error('클래스 정보들 불러오기 오류 => ', error);
--- src/components/main/BestClass.tsx
+++ src/components/main/BestClass.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 import ClassCard from './ClassCard';
+import { useClassInfoStore } from '@/store/ClassInfoStore';
+import { fetchClassInfos } from '@/api/supabase/fetchClassInfo';
 const BestClass = () => {
+  const { classInfos, setClassInfos } = useClassInfoStore();
+
+  useEffect(() => {
+    const getClassInfos = async () => {
+      const infos = await fetchClassInfos();
+      setClassInfos(infos);
+    };
+    getClassInfos();
+  }, []);
+
   return (
     <div>
-      BestClass
-      <div className="w-[85vw] flex justify-between p-2 items-center bg-slate-500">
+      <p>BestClass</p>
+      <div className="w-[85vw] overflow-auto flex justify-between p-2 items-center bg-slate-500">
         <button className="btn btn-circle "> </button>
-        <ClassCard />
+        {classInfos.map((info, classId) => (
+          <ClassCard key={classId} classInfos={info} />
+        ))}
         <button className="btn btn-circle "> </button>
       </div>
     </div>
