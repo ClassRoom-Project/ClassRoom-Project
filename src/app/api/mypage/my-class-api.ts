d9594512dfa73ace164b20f08d458ca7bc316f4d
HyunjinKim <koujie11@gmail.com>
2024-04-01 14:11:34 +0900

    fix : 함수 스펠링 수정 #47

--- src/app/api/mypage/my-class-api.ts
+++ src/app/api/mypage/my-class-api.ts
@@ -1,11 +1,11 @@
 import { userId } from '@/app/mypage/page';
 
 import { PostgrestResponse } from '@supabase/supabase-js';
-import { MyRegistedClassType } from '@/types/class';
+import { MyRegisteredClassType } from '@/types/class';
 import { supabase } from '@/app/api/supabase/supabase';
 
-export const getMyRegistedClass = async () => {
-  const { data: myClassInfo, error }: PostgrestResponse<MyRegistedClassType> = await supabase
+export const getMyRegisteredClass = async () => {
+  const { data: myClassInfo, error }: PostgrestResponse<MyRegisteredClassType> = await supabase
     .from('class')
     .select('user_id, class_id, title, location,detailLocation, date, time, image, active')
     .eq('user_id', userId);
