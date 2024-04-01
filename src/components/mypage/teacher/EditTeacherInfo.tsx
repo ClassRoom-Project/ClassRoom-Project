6e268e21f022ee0acf61b383ba95516e13ff4390
HyunjinKim <koujie11@gmail.com>
2024-04-01 14:12:05 +0900

    feat : 선생님 정보 수정하기 - 은행 정보 UI 구현

--- src/components/mypage/teacher/EditTeacherInfo.tsx
+++ src/components/mypage/teacher/EditTeacherInfo.tsx
@@ -2,14 +2,34 @@ import { CancleButton, EditButton } from '@/components/common/mypage/buttons';
 import Image from 'next/image';
 import React, { useId } from 'react';
 import BasicProfileImage from '../../../../public/profile-image.png';
+import { FieldType, JobType } from '@/types/user';
 
 const EditTeacherInfo = () => {
   // id와 htmlFor 연결 => useId 내장 훅 사용
   const jobId = useId();
   const fieldId = useId();
+  const bankId = useId();
 
-  const jobOptions = ['교사', '개발자', '예술가'];
-  const businessFieldOptions = ['교육', 'IT', '예술'];
+  const jobOptions: JobType[] = ['교사', '요리사', '개발자', '운동선수', '음악가', '예술가', '뷰티'];
+  const businessFieldOptions: FieldType[] = ['교육', '요리', 'IT', '스포츠', '피아니스트', '공방', '애견미용'];
+  const koreanBanks = [
+    '국민은행',
+    '우리은행',
+    '신한은행',
+    '하나은행',
+    '기업은행',
+    '농협은행',
+    'KEB하나은행',
+    'SC제일은행',
+    '씨티은행',
+    'BNK경남은행',
+    '광주은행',
+    '대구은행',
+    '부산은행',
+    '전북은행',
+    '제주은행',
+    '카카오뱅크'
+  ];
 
   return (
     <div className="flex">
@@ -42,6 +62,22 @@ const EditTeacherInfo = () => {
               })}
             </select>
           </form>
+          <form className="m-4 p-4 flex gap-4">
+            <label id={bankId}>은행</label>
+            <select name="job" id={bankId} className="border w-40">
+              {koreanBanks.map((bank) => {
+                return (
+                  <option value={bank} key={bank}>
+                    {bank}
+                  </option>
+                );
+              })}
+            </select>
+          </form>
+          <div className="m-4 p-4 flex gap-4">
+            <span>계좌 정보</span>
+            <input type="text" className="border" />
+          </div>
           <div className="m-4 p-4 flex gap-4">
             <span>총 수익</span>
             <input type="text" className="border" />
