b1aa33bc939d94d6ac2605ae9f1bd873bc00c9b0
HyunjinKim <koujie11@gmail.com>
2024-04-01 15:26:51 +0900

    feat : 선생님 정보 수정 - 은행 정보 추가하기 구현 중 #47

--- src/components/mypage/teacher/EditTeacherInfo.tsx
+++ src/components/mypage/teacher/EditTeacherInfo.tsx
@@ -1,10 +1,12 @@
 import { CancleButton, EditButton } from '@/components/common/mypage/buttons';
 import Image from 'next/image';
-import React, { useId } from 'react';
+import React, { useId, useState } from 'react';
 import BasicProfileImage from '../../../../public/profile-image.png';
 import { FieldType, JobType } from '@/types/user';
 
 const EditTeacherInfo = () => {
+  const [isEditing, setIsEditing] = useState(false);
+
   // id와 htmlFor 연결 => useId 내장 훅 사용
   const jobId = useId();
   const fieldId = useId();
@@ -30,6 +32,29 @@ const EditTeacherInfo = () => {
     '제주은행',
     '카카오뱅크'
   ];
+
+  // 수정하기 버튼 -> supabase에 수정한 정보 update
+  const handleOnClickEditTeacherInfoBtn = () => {
+    // 수정된 사항이 없는 경우
+    // const isNicknameChanged = newNickname !== userInfo?.nickname;
+    // const isEmailChanged = newEmail !== userInfo?.email;
+    // const isNewPasswordChanged = newPassword !== userInfo?.password;
+
+    // if (!isNicknameChanged && !isEmailChanged && !isNewPasswordChanged) {
+    //   alert('수정 사항이 없습니다.');
+    //   return;
+    // }
+
+    // // 수정된 사항이 있는 경우
+    // updateUserInfoMutation({ newNickname, newEmail, newPassword });
+    alert('프로필 수정이 완료되었습니다.');
+  };
+
+  // 취소하기 버튼
+  const handleOnClickCancleBtn = () => {
+    setIsEditing(false);
+    alert('프로필 수정이 취소 되었습니다. ');
+  };
 
   return (
     <div className="flex">
@@ -76,7 +101,7 @@ const EditTeacherInfo = () => {
           </form>
           <div className="m-4 p-4 flex gap-4">
             <span>계좌 정보</span>
-            <input type="text" className="border" />
+            {isEditing ? <input type="text" className="border p-1" /> : <p>******</p>}
           </div>
           <div className="m-4 p-4 flex gap-4">
             <span>총 수익</span>
@@ -84,8 +109,18 @@ const EditTeacherInfo = () => {
           </div>
         </div>
         <div className="p-4 flex gap-4">
-          <EditButton />
-          <CancleButton />
+          {isEditing ? (
+            <button onClick={handleOnClickEditTeacherInfoBtn} className="p-4 border rounded-xl w-[150px]">
+              수정 완료
+            </button>
+          ) : (
+            <button onClick={() => setIsEditing(true)} className="p-4 border rounded-xl w-[150px]">
+              수정하기
+            </button>
+          )}
+          <button onClick={handleOnClickCancleBtn} className="p-4 border rounded-xl w-[150px]  bg-rose-500 text-white">
+            취소하기
+          </button>
         </div>
       </div>
     </div>
