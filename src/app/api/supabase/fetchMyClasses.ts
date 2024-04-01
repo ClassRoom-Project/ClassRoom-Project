f7a8094bafb56455a3086b92a4a08804475b584b
limhyerin <rin091345@gmail.com>
2024-04-01 20:03:36 +0900

    refactor: Image대신 img 변경 #34

--- src/app/api/supabase/fetchMyClasses.ts
+++ src/app/api/supabase/fetchMyClasses.ts
@@ -1,37 +1,48 @@
 import { supabase } from './supabase';
 
-async function fetchMyClasses(userId:string) {
-  const { data: reserves, error: reservesError } = await supabase
-    .from('reserve')
-    .select('reserved_at, reserve_date, class_id, reserve_id')
-    .eq('user_id', userId)
+// async function fetchMyClasses(userId:string) {
+//   const { data: reserves, error: reservesError } = await supabase
+//     .from('reserve')
+//     .select('reserved_at, reserve_date, class_id, reserve_id')
+//     .eq('user_id', userId)
 
-  if (reservesError) {
-    console.error(reservesError)
-    return
+//   if (reservesError) {
+//     console.error(reservesError)
+//     return
+//   }
+
+//   const classes = [];
+
+//   for (const reserve of reserves) {
+//     const { data: classData, error: classError } = await supabase
+//       .from('class')
+//       .select('image, title')
+//       .eq('class_id', reserve.class_id)
+//       .limit(1); // 이미지는 0번째 값만 가져오도록 제한
+
+//     if (classError) {
+//       console.error(classError)
+//       continue;
+//     }
+
+//     classes.push({
+//       ...reserve,
+//       ...classData[0]
+//     });
+//   }
+
+//   return classes;
+// }
+
+export async function fetchMyClasses(userId:string) {
+  const { data, error } = await supabase
+    .rpc('fetch_my_reserved_classes_for_user', { p_user_id: userId }); // 파라미터 이름을 p_user_id로 변경
+
+  if (error) {
+    console.error(error);
+    return [];
   }
-
-  const classes = [];
-
-  for (const reserve of reserves) {
-    const { data: classData, error: classError } = await supabase
-      .from('class')
-      .select('image, title')
-      .eq('class_id', reserve.class_id)
-      .limit(1); // 이미지는 0번째 값만 가져오도록 제한
-
-    if (classError) {
-      console.error(classError)
-      continue;
-    }
-
-    classes.push({
-      ...reserve,
-      ...classData[0]
-    });
-  }
-
-  return classes;
+  return data;
 }
 
 // 예약 취소 함수
--- src/components/mypage/student/MyReservedClass.tsx
+++ src/components/mypage/student/MyReservedClass.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-import MyClassImage from '../../../../public/class-sample-img.jpeg';
 import fetchMyClasses, { cancelReservation } from '@/app/api/supabase/fetchMyClasses';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { ClassItem } from '@/types/register';
 
 const MyReservedClass = () => {
   const [classes, setClasses] = useState<ClassItem[]>([]); // 리액트 쿼리로 개선 가능
-  const router = useRouter();
 
   useEffect(() => {
     async function fetchData() {
       const data = await fetchMyClasses('523e4567-e89b-12d3-a456-426614174005');
-      setClasses(data || []);
+      const formattedData = data.map((classItem:ClassItem) => ({
+        ...classItem,
+        image: classItem.image?.replace(/["{}]/g, '') || '' // 잘못된 따옴표 제거
+      }));
+      setClasses(formattedData || []);
     }
     fetchData();
   }, []);
@@ -38,8 +39,8 @@ const MyReservedClass = () => {
       {classes.map((classItem) => (
         <article key={classItem.reserve_id} className="flex gap-4">
           <div className="w-[300px] h-[200px]">
-            <Image
-              src={classItem.image || MyClassImage}
+            <img
+              src={classItem.image || '/class-sample-img.jpeg'}
               alt="클래스 대표 사진"
               width={300}
               height={200}
@@ -59,14 +60,9 @@ const MyReservedClass = () => {
             <section className="flex gap-4 m-4">
               <button className="border rounded-xl p-4 w-[150px] bg-rose-500 text-white" onClick={() => handleCancelReservation(classItem.reserve_id)}>예약 취소하기</button>
               <button className="border rounded-xl p-4 w-[150px]">문의하기</button>
-              <button
-                onClick={() => {
-                  router.push(`list/detail/${classItem.class_id}`);
-                }}
-                className="border rounded-xl p-4 w-[150px]"
-                >
-                클래스 보러가기
-              </button>
+              <Link href={`list/detail/${classItem.class_id}`}>
+                <div className="border rounded-xl p-4 w-[150px] cursor-pointer">클래스 보러가기</div>
+              </Link>
             </section>
           </div>
         </article>
