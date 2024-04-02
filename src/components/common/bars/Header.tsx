b7429795e5fb512bafa073581565c91ed57f351e
ocean <twyeo9006@gmail.com>
2024-04-02 20:48:53 +0900

    feat:SearchClass 위치사이드바->헤더 폼태그 작업완료

--- src/components/common/bars/Header.tsx
+++ src/components/common/bars/Header.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 import React, { PropsWithChildren } from 'react';
 import defaultImage from '../../../../public/profile-image.png';
 import Link from 'next/link';
+import { SearchClass } from './categories/SearchClass';
 
 const Header = ({ children }: PropsWithChildren) => {
   return (
     <>
       <div className="flex p-[15px]  justify-between items-center h-[60px] border-b-[1px] border-solid border-gray-300">
         <Link href={'http://localhost:3000/'}>Logo</Link>
+        <SearchClass />
         <div className="flex justify-center items-center">
           <div className="mr-[10px]">alarm</div>
           <Link href={'http://localhost:3000/mypage'}>
