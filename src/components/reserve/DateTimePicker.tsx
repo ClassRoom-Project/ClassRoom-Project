d4e418c60e71ab5b0a95d2f88f73d700c2b34e51
Jiwon <tj1tj2tj3@naver.com>
2024-04-04 00:17:06 +0900

    feat: 24시간제 -> 12시간제 변환하여 출력 #79

--- src/components/reserve/DateTimePicker.tsx
+++ src/components/reserve/DateTimePicker.tsx
@@ -8,21 +8,22 @@ import { CaptionProps, DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import './day-picker.css';
 
-// import { SlArrowLeft } from 'react-icons/sl';
-// import { SlArrowRight } from 'react-icons/sl';
-
 const DateTimePicker = ({ classDateList, classTimeList }: { classDateList: string[]; classTimeList: string[] }) => {
   const setReserveInfo = useReserveStore((state) => state.setReserveInfo);
 
+  const today = new Date();
   const [selectedTime, setSelectedTime] = useState(classTimeList[0]);
+  const [selectedDate, setSelectedDate] = useState<string>(classDateList[0]);
+
+  useEffect(() => {
+    setReserveInfo({ reserveDate: selectedDate, reserveTime: selectedTime });
+  }, [selectedDate, selectedTime, setReserveInfo]);
 
   const handleTimeClick = (time: string) => {
     setSelectedTime(time);
   };
 
   // 리액트 데이피커 ------------------------------------------------------------------
-  const today = new Date();
-  const [selectedDate, setSelectedDate] = useState<string>(classDateList[0]);
 
   // 날짜 클릭시 set
   const handleDateChange = (newDate: Date | undefined) => {
@@ -50,9 +51,28 @@ const DateTimePicker = ({ classDateList, classTimeList }: { classDateList: strin
     return <div className="flex justify-center">{format(props.displayMonth, 'uuuu년 LLLL', { locale: ko })}</div>;
   }
 
-  useEffect(() => {
-    setReserveInfo({ reserveDate: selectedDate, reserveTime: selectedTime });
-  }, [selectedDate, selectedTime, setReserveInfo]);
+  // 24시간제를 12시간제로 변환하는 함수
+  const ConvertTimeTo12HourClock = (classTime: string) => {
+    const hour = Number(classTime.slice(0, 2));
+    const minute = classTime.slice(2, 5);
+
+    // 시간이 12 초과 (오후)
+    if (hour > 12) {
+      const pmHour = hour - 12;
+      const formatHour = pmHour < 10 ? '0' + pmHour : pmHour;
+      return formatHour + minute + ' PM';
+
+      // 시간이 12 미만 (오전) && 00시가 아닐 때
+    } else if (hour < 12 && hour !== 0) {
+      const amHour = hour;
+      const formatHour = amHour < 10 ? '0' + amHour : amHour;
+      return formatHour + minute + ' AM';
+
+      // 00시, 12시일 경우
+    } else {
+      return hour === 0 ? 12 + minute + ' AM' : 12 + minute + ' PM';
+    }
+  };
 
   return (
     <div className="w-2/5 flex flex-col gap-4">
@@ -85,7 +105,7 @@ const DateTimePicker = ({ classDateList, classTimeList }: { classDateList: strin
                   time === selectedTime ? 'bg-rose-200' : 'bg-white'
                 } tracking-wide rounded-lg`}
               >
-                {time}
+                {ConvertTimeTo12HourClock(time)}
               </button>
             );
           })}
