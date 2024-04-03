cbed84cf00d76820f3f9621369a530184ab9510f
HyunjinKim <koujie11@gmail.com>
2024-04-03 17:34:17 +0900

    refact : table name 변경

--- src/app/api/mypage/user-api.ts
+++ src/app/api/mypage/user-api.ts
@@ -1,12 +1,13 @@
-import { PostgrestMaybeSingleResponse } from '@supabase/supabase-js';
+import { PostgrestMaybeSingleResponse, PostgrestResponse, PostgrestSingleResponse } from '@supabase/supabase-js';
 import { supabase } from '../supabase/supabase';
-import { UpdateTeacherInfoType, UpdateUserInfoType, UserType } from '@/types/user';
+import { UpdateTeacherInfoType, UpdateUserInfoType, UserInfoType, UserType } from '@/types/user';
 import { userId } from '@/app/(clrm)/mypage/page';
+import { useUserStore } from '@/store/UserInfoStore';
 
 // User가 선생님인지 수강생인지 구분 : isTeacher 값 불러오기
 export const getUserRole = async ({ userId }: { userId: string }) => {
   const { data: userRole, error }: PostgrestMaybeSingleResponse<UserType> = await supabase
-    .from('user')
+    .from('users')
     .select('isTeacher')
     .eq('user_id', userId)
     .single();
@@ -20,9 +21,9 @@ export const getUserRole = async ({ userId }: { userId: string }) => {
 
 // User(선생님/수강생) 정보 불러오기
 export const getUserInfo = async () => {
-  const { data: userInfo, error }: PostgrestMaybeSingleResponse<UserType> = await supabase
-    .from('user')
-    .select('nickname, email, password, profile_image')
+  const { data: userInfo, error }: PostgrestMaybeSingleResponse<UserInfoType> = await supabase
+    .from('users')
+    .select('nickname, email, profile_image')
     .eq('user_id', userId)
     .single();
 
@@ -30,12 +31,17 @@ export const getUserInfo = async () => {
     console.error(error);
   }
 
+  useUserStore.getState().setUserInfo(userInfo);
+
   return userInfo;
 };
 
 // User(선생님/수강생) 정보 수정하기 : supabase에 update
-export const updateUserInfo = async ({ newNickname }: UpdateUserInfoType) => {
-  const { data, error } = await supabase.from('user').update({ nickname: newNickname }).eq('user_id', userId);
+export const updateUserInfo = async ({ newNickname, newProfileImage }: UpdateUserInfoType) => {
+  const { data, error } = await supabase
+    .from('users')
+    .update({ nickname: newNickname, profile_image: newProfileImage })
+    .eq('user_id', userId);
   if (error) {
     console.error(error);
   }
@@ -45,7 +51,7 @@ export const updateUserInfo = async ({ newNickname }: UpdateUserInfoType) => {
 // User(선생님/수강생) nickname 중복 확인하기
 export const checkUserNickname = async ({ newNickname }: Pick<UpdateUserInfoType, 'newNickname'>) => {
   const { data, error } = await supabase
-    .from('user')
+    .from('users')
     .select('nickname')
     .not('user_id', 'eq', userId)
     .eq('nickname', newNickname);
@@ -61,7 +67,7 @@ export const checkUserNickname = async ({ newNickname }: Pick<UpdateUserInfoType
 // User(선생님/수강생) 정보 불러오기
 export const getTeacherInfo = async () => {
   const { data: teacherInfo, error }: PostgrestMaybeSingleResponse<UserType> = await supabase
-    .from('user')
+    .from('users')
     .select('job, field, bank, account')
     .eq('user_id', userId)
     .single();
@@ -69,7 +75,7 @@ export const getTeacherInfo = async () => {
   if (error) {
     console.error(error);
   }
-  // console.log('teacherInfo', teacherInfo);
+
   return teacherInfo;
 };
 
@@ -81,7 +87,7 @@ export const updateTeacherInfo = async ({
   account
 }: UpdateTeacherInfoType) => {
   const { data, error } = await supabase
-    .from('user')
+    .from('users')
     .update({ job: newSelectedJob, field: newSelectedField, bank: selectedBank, account: account })
     .eq('user_id', userId);
   if (error) {
