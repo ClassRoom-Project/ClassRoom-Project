e3b3f6abebc00a48c2ef3ec132586db5b3695a87
HyunjinKim <koujie11@gmail.com>
2024-04-06 10:25:44 +0900

    style : sidebar / header css 수정

--- src/components/common/bars/Header.tsx
+++ src/components/common/bars/Header.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 import Image from 'next/image';
 import React, { PropsWithChildren } from 'react';
 import defaultImage from '../../../../public/profile-image.png';
 import Link from 'next/link';
 import LoginState from '@/components/login/LoginState';
 import { SearchClass } from './categories/SearchClass';
+import { useUserStore } from '@/store/UserInfoStore';
+import { LuBell } from 'react-icons/lu';
 
 const Header = ({ children }: PropsWithChildren) => {
+  const { userInfo } = useUserStore();
+
   return (
     <>
       <div className="flex p-[15px]  justify-between items-center h-[60px] border-b-[1px] border-solid border-gray-300">
         <Link href={'http://localhost:3000/'}>Logo</Link>
         <SearchClass />
         <div className="flex justify-center items-center">
-          <div className="mr-[10px]">alarm</div>
+          <div className="mr-[10px]">
+            <LuBell size={30} />
+          </div>
+          <p className="p-4">{userInfo?.nickname} 님</p>
           <Link href={'http://localhost:3000/mypage'}>
-            <Image src={defaultImage} alt="Profile image" className="mr-[5px] h-[50px] w-[50px]" />
+            <img
+              src={userInfo?.profile_image}
+              alt="Profile image"
+              className="mr-[5px] rounded-full"
+              width={50}
+              height={50}
+            />
           </Link>
         </div>
         <LoginState />
