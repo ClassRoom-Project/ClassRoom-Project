de8daad38c813b1532f2f2f09e4132e3f9e3b6ed
HyunjinKim <koujie11@gmail.com>
2024-04-06 23:16:33 +0900

    refact : 헤더 프로필 이미지 없을 때 기본 프로필이미지 보여주기

--- src/components/common/bars/Header.tsx
+++ src/components/common/bars/Header.tsx
@@ -2,16 +2,19 @@
 
 import Image from 'next/image';
 import React, { PropsWithChildren } from 'react';
-import defaultImage from '../../../../public/profile-image.png';
 import Link from 'next/link';
 import LoginState from '@/components/login/LoginState';
 import { SearchClass } from './categories/SearchClass';
-import { useUserStore } from '@/store/UserInfoStore';
 import { LuBell } from 'react-icons/lu';
+
+import basicProfileImage from '../../../../public/profile-image.png';
+import { useUserStore } from '@/store/userInfoStore';
 
 const Header = ({ children }: PropsWithChildren) => {
   const { userInfo } = useUserStore();
 
+  // 프로필 이미지가 없을 때, 기본 프로필 이미지 보여주기
+  const profileImage = userInfo?.profile_image ? userInfo?.profile_image : basicProfileImage;
   return (
     <>
       <div className="flex p-[15px]  justify-between items-center h-[60px] border-b-[1px] border-solid border-gray-300">
@@ -23,12 +26,13 @@ const Header = ({ children }: PropsWithChildren) => {
           </div>
           <p className="p-4">{userInfo?.nickname} 님</p>
           <Link href={'http://localhost:3000/mypage'}>
-            <img
-              src={userInfo?.profile_image}
+            <Image
+              src={profileImage}
               alt="Profile image"
               className="mr-[5px] rounded-full"
               width={50}
               height={50}
+              unoptimized={true}
             />
           </Link>
         </div>
