0b7a5235a4f359f9a30a1387b9a172c6269ffc76
ocean <twyeo9006@gmail.com>
2024-04-07 19:22:41 +0900

    feat: 클래스 디테일 페이지 태그들 추가,fix : ClassAllType 오타 개선 max_peple -> max_people

--- src/components/classDetail/ClassDetailRight.tsx
+++ src/components/classDetail/ClassDetailRight.tsx
@@ -1,15 +1,15 @@
 import { ClassAllType } from '@/types/class';
-
+import ClassInfos from './ClassInfos';
 const ClassDetailRight = ({ classData }: { classData: ClassAllType | null }) => {
   return (
-    <div className="flex flex-col div-5 ml-4 justify-center items-center w-[500px] rounded-lg min-h-full border-[#5373FF] border-solid border-[1px]">
+    <div className="flex flex-col p-5 ml-4 justify-center items-center w-[500px] rounded-lg min-h-full border-[#5373FF] border-solid border-[1px]">
       <div className="flex items-center mt-2 justify-center w-[350px]">
         <p className=" text-[#5373FF]">{classData?.title}</p>
       </div>
       <div className="w-[350px] h-[350px] border-gray-400 border-solid border-b mt-2">
         <p>{classData?.description}</p>
       </div>
-      <div className="w-[350px] mt-2">class infos</div>
+      <div className="w-[350px] mt-2">{classData?.class_id ? <ClassInfos classId={classData.class_id} /> : null} </div>
       <div className="w-[350px] mt-2">button</div>
     </div>
   );
---
+++ src/components/classDetail/ClassInfoBox.tsx
@@ -0,0 +1,7 @@
+export const ClassInfoBox = ({ classInfos }: { classInfos: number | string | undefined }) => {
+  return (
+    <div className="rounded-2xl border-[#5373FF] border-solid border-[1px] p-1 flex justify-center items-center">
+      {classInfos}
+    </div>
+  );
+};
