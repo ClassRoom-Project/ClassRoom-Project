96d0145276dd2ad65d0d832ef3c71872fdb545d0
HyunjinKim <koujie11@gmail.com>
2024-04-09 23:12:11 +0900

    merge : get code

--- src/components/login/LoginState.tsx
+++ src/components/login/LoginState.tsx
@@ -4,26 +4,30 @@ import useLoginUserId from '@/hooks/useLogin/useLoginUserId';
 import { useReadLoginUserId } from '@/hooks/useLogin/useSetEmailToApi';
 import useSetSessionStorage from '@/hooks/useLogin/useSetStorage';
 import useSessionStorageUserEmail from '@/hooks/useLogin/useSessionStorageUserEmail';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import useUserEmail from '@/hooks/useLogin/useUserEmail';
+import { Suspense } from 'react';
 
 export default function LoginState() {
-  const userEmail = useUserEmail();
-  // const userEmail = useSessionStorageUserEmail();
-  // const userEmail = useSetSessionStorage();
-  // console.log('userEmail', userEmail);
-  // const userEmail = typeof window !== 'undefined' ? sessionStorage.getItem('userEmail') : null;
+  const { data: session, status } = useSession();
+
+  const userEmail = session?.user?.email ?? null;
+
+  useSetSessionStorage();
+  useReadLoginUserId(userEmail);
+  useLoginUserId({ userEmail });
 
   const handleLogout = async () => {
     sessionStorage.removeItem('userEmail');
     await signOut({ redirect: true, callbackUrl: '/' });
   };
 
-  // 훅 호출
-  useSetSessionStorage();
-  useReadLoginUserId(userEmail);
-  useLoginUserId({ userEmail });
+  if (status === 'authenticated') {
+    return <div>{userEmail ? <button onClick={handleLogout}>Logout</button> : <Link href="/hello">Login</Link>}</div>;
+  }
 
+  if (status === 'loading') {
+    return <p>Logout</p>;
+  }
   return <div>{userEmail ? <button onClick={handleLogout}>Logout</button> : <Link href="/hello">Login</Link>}</div>;
 }
