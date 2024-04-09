42a456c38f46820e6a2b90b2cd3befe6204e8d13
Jiwon <tj1tj2tj3@naver.com>
2024-04-10 00:46:24 +0900

    style: 클래스 정보, 연락처 틀잡기 #93

--- src/app/(clrm)/reserve/page.tsx
+++ src/app/(clrm)/reserve/page.tsx
@@ -10,21 +10,60 @@ export default async function ReservePage({ searchParams }: { searchParams: { cl
   const classInfo = await newFetchReserveClassInfo(classId);
 
   return (
-    <div className="w-full h-full">
-      <h1 className="text-xl">예약하기</h1>
-      {classInfo ? (
-        <div className="flex w-full h-full bg-gray-200 p-6">
-          <DateTimePicker classDates={classInfo.dates} />
-          <div className="flex flex-col justify-between items-center w-full p-6">
+    <>
+      <h1 className="text-xl">클래스 상세보기</h1>
+      <div className="w-full min-h-screen bg-violet-100 flex justify-center items-center flex-col">
+        {classInfo ? (
+          <div className="flex w-2/3 h-full bg-violet-200">
+            <div className="flex flex-col w-2/5 bg-gray-2006">
+              <div className="p-6 pb-12 bg-white mb-6 rounded-md  border border-black border-solid ">
+                <p>선택하신 클래스</p>
+                <div className="w-full p-4 h-[250px] border border-solid border-black mb-4">사진자리</div>
+                <div className="px-2 flex-col flex gap-1">
+                  <h1 className="font-bold text-xl mb-4">클래스 타이틀</h1>
+                  <p className="font-bold">
+                    클래스 유형 : <span className="font-normal">원데이 ㅋㅋ</span>
+                  </p>
+                  <p className="font-bold">
+                    클래스 소요시간 : <span className="font-normal">원데이 ㅋㅋ</span>
+                  </p>
+                  <p className="font-bold">
+                    클래스 난이도 : <span className="font-normal">원데이 ㅋㅋ</span>
+                  </p>
+                  <p className="font-bold">
+                    위치: <span className="font-normal">원데이 ㅋㅋ</span>
+                  </p>
+                  <p className="font-bold">
+                    1인당 수강 금액 : <span className="font-normal">원데이 ㅋㅋ</span>
+                  </p>
+                </div>
+              </div>
+              <div className="rounded-md flex flex-col bg-white border border-black border-solid p-6">
+                <p className="mb-6 text-lg font-bold ">연락처 입력</p>
+                <div className="flex flex-col  mb-4">
+                  <p className="font-bold mb-1">계정 ID (알림 메일이 발송됩니다.)</p>
+                  <p>seifei@fsmfkl</p>
+                </div>
+                <div className="flex flex-col">
+                  <p className="font-bold mb-1">이름 (닉네임)</p>
+                  <p>망고젤리</p>
+                </div>
+                <button className="btn btn-ghost border border-solid border-gray-300 text-gray-500 h-[20px]">
+                  연락처 수정하기
+                </button>
+              </div>
+            </div>
+            <div>시간인원 선택</div>
+            {/* <DateTimePicker classDates={classInfo.dates} />
             <ClassInfo classInfo={classInfo} />
             <CurrentReserveQuantity classId={classInfo.classId} maxPeople={classInfo?.maxPeople} />
             <PriceCalculator price={classInfo.price} classId={classInfo.classId} maxPeople={classInfo.maxPeople} />
-            <ReserveButton classId={classInfo.classId} maxPeople={classInfo.maxPeople} />
+            <ReserveButton classId={classInfo.classId} maxPeople={classInfo.maxPeople} /> */}
           </div>
-        </div>
-      ) : (
-        <p>클래스 정보를 불러오는데 오류가 발생했습니다. 잠시 후 다시 시도해주세요.</p>
-      )}
-    </div>
+        ) : (
+          <p>클래스 정보를 불러오는데 오류가 발생했습니다. 잠시 후 다시 시도해주세요.</p>
+        )}
+      </div>
+    </>
   );
 }
