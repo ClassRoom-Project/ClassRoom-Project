ad02422e782b3fd495032c6a44fa5551c641b143
Jiwon <tj1tj2tj3@naver.com>
2024-04-12 18:42:25 +0900

    feat: 리다이렉트..

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,7 +1,13 @@
 import { redirect } from 'next/dist/server/api-utils';
 import { insertNewReservation } from '../reserve/insertNewReservation';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(request: any) {
+type RedirectEntry = {
+  destination: string;
+  permanent: boolean;
+};
+
+export async function GET(request: NextRequest) {
   // console.log(request);
   const { searchParams } = new URL(request.url);
 
@@ -10,9 +16,9 @@ export async function GET(request: any) {
   const paymentKey = searchParams.get('paymentKey');
   const amount = searchParams.get('amount');
 
-  console.log('🚀 ~ GET ~ orderId:', orderId);
-  console.log('🚀 ~ GET ~ amount:', amount);
-  console.log('🚀 ~ GET ~ paymentKey:', paymentKey);
+  // console.log('🚀 ~ GET ~ orderId:', orderId);
+  // console.log('🚀 ~ GET ~ amount:', amount);
+  // console.log('🚀 ~ GET ~ paymentKey:', paymentKey);
 
   const response = await fetch('https://api.tosspayments.com/v1/payments/confirm', {
     method: 'POST',
@@ -26,9 +32,5 @@ export async function GET(request: any) {
   const data = await response.json(); // Properly handling JSON conversion
   console.log(data);
 
-  return response.json();
-
-  // redirect('success')
-
-  // response.redirect(`/success?orderId=${orderId}}`);
+  return NextResponse.redirect(new URL(`http://localhost:3000/reserve/success/${data.orderId}`));
 }
