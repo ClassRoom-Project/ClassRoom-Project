fc0cafd14a06db4edfd7d9860eac6f6b8d1e93b1
Jiwon <tj1tj2tj3@naver.com>
2024-04-14 15:53:31 +0900

    refactor: 배포 환경변수 설정 중 #154

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,21 @@
 const nextConfig = {
   // 외부 image 파일을 불러와서 쓰려면 이미지 호스트 허용 목록에 추가해야함.
   images: {
-    domains: [
-      'img.freepik.com',
-      'hdurwturhsczrdeugmon.supabase.co',
-      'd1x9f5mf11b8gz.cloudfront.net',
-      'http://localhost:3000/messages',
-      'encrypted-tbn3.gstatic.com',
-      'encrypted-tbn0.gstatic.com',
-      'lh3.googleusercontent.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
     ]
+    // domains: [
+    //   'img.freepik.com',
+    //   'hdurwturhsczrdeugmon.supabase.co',
+    //   'd1x9f5mf11b8gz.cloudfront.net',
+    //   'http://localhost:3000/messages',
+    //   'encrypted-tbn3.gstatic.com',
+    //   'encrypted-tbn0.gstatic.com',
+    //   'lh3.googleusercontent.com'
+    // ]
   }
 };
 
