f3066ae55215ca88ae52513fe32097eb134d4659
ocean <twyeo9006@gmail.com>
2024-04-14 14:58:26 +0900

    feat : 검색창 쓰로틀링적용

--- src/components/common/bars/categories/SearchClass.tsx
+++ src/components/common/bars/categories/SearchClass.tsx
@@ -1,13 +1,20 @@
 import { useSearchStore } from '@/store/classFilterStore';
 import { useRouter } from 'next/navigation';
 import { IoIosSearch } from 'react-icons/io';
+import _ from 'lodash';
+//yarn add --dev @types/lodash
+//yarn add lodash
 
 export const SearchClass = () => {
   const router = useRouter();
   const { selectedTitle, setSelectedTitle } = useSearchStore();
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedTitle(e.target.value);
+    console.log(e.target.value);
   };
+
+  //Todo: 쓰로틀링 잘 적용안되는 에러 해결
+  const throttling = _.throttle(handleSearchChange, 300);
   const handleSearchBtn = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     router.push('/list');
@@ -16,13 +23,13 @@ export const SearchClass = () => {
     <form className="h-[120px] w-[300px] flex items-center justify-center" onSubmit={handleSearchBtn}>
       <div className="border-[1px] rounded-xl w-64 items-end justify-end border-main-color">
         <input
-          onChange={handleSearchChange}
+          onChange={throttling}
           value={selectedTitle}
-          className="ml-2"
+          className="h-12 rounded-xl"
           type="text"
           placeholder="  클래스명"
         />
-        <button type="submit" className="btn w-[66px] rounded-xl text-white bg-main-color hover:bg-button-hover-color">
+        <button type="submit" className="btn w-[72px] rounded-xl text-white bg-main-color hover:bg-button-hover-color">
           <IoIosSearch />
         </button>
       </div>
