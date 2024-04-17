d29ef246fe2c99287357e2c105d0b73078dad611
ocean <twyeo9006@gmail.com>
2024-04-17 15:51:51 +0900

    style : 클래스디테일페이지 댓글 UI구현

--- src/app/api/classdetail/detailComment.ts
+++ src/app/api/classdetail/detailComment.ts
@@ -1,9 +1,14 @@
 import { getLoginUserType } from '@/types/authUser/authUserTypes';
 import { supabase } from '../supabase/supabase';
+import { comment } from 'postcss';
 
 //댓글 불러오기 api, rpc로 쿼리문 작성해야된다 이렇게하니까 안나온다 데이터가
-export const getDetailComment = async (classId: string | undefined) => {
-  const { data: comments, error } = await supabase.rpc('get_comments_with_user_info', { input_class_id: classId });
+export const getDetailComment = async (classId: string | undefined, page = 1, limit_val = 5) => {
+  const { data: comments, error } = await supabase.rpc('get_comments_with_user_info', {
+    input_class_id: classId,
+    page: page,
+    limit_val: limit_val
+  });
 
   if (error) {
     console.error('댓글 불러오기 오류 -->', error);
@@ -18,7 +23,8 @@ export const createDetailComment = async (
   classId: string | undefined,
   star: number | undefined,
   userId: string | undefined,
-  content: string | undefined
+  content: string | undefined,
+  commentImage: string | undefined
 ) => {
   const { data: comments, error } = await supabase.from('comments').insert([
     {
@@ -26,7 +32,8 @@ export const createDetailComment = async (
       user_id: userId,
       content: content,
       star: star,
-      create_at: new Date()
+      create_at: new Date(),
+      commentImage: commentImage
     }
   ]);
 
--- src/components/classDetail/CreateComments.tsx
+++ src/components/classDetail/CreateComments.tsx
@@ -9,6 +9,10 @@ import { useSession } from 'next-auth/react';
 import { getUserIdByEmail } from '@/app/api/userEmail/loginUserId';
 import { ListDetailClassInfo } from '@/types/class';
 import { useLoginStore } from '@/store/login/loginUserIdStore';
+import { ImageFileWithPreview } from '@/types/register';
+import { supabase } from '@/app/api/supabase/supabase';
+import Image from 'next/image';
+import noImage from '@/assets/images/no_img.jpg';
 
 //Todo : 예약한 사람만 댓글 입력가능하게 하기 , 댓글 수정삭제 구현 ,사진 기능
 const CreateComments = ({ classData }: { classData: ListDetailClassInfo | null }) => {
@@ -17,13 +21,10 @@ const CreateComments = ({ classData }: { classData: ListDetailClassInfo | null }
   const { data: session } = useSession();
   const queryClient = useQueryClient();
   const { loginUserId } = useLoginStore();
+  const [commentImage, setCommentImage] = useState<ImageFileWithPreview[]>([]);
 
   const email: string = session?.user?.email ?? '';
-  const {
-    data: userData,
-    error: userDataError,
-    status: userDataStatus
-  } = useQuery({
+  const { data: userData } = useQuery({
     queryKey: ['getUserIdByEmail'],
     queryFn: () => getUserIdByEmail(email),
     enabled: !!email
@@ -31,9 +32,27 @@ const CreateComments = ({ classData }: { classData: ListDetailClassInfo | null }
 
   const userId: string | undefined = userData?.user_id;
 
+  // 파일 업로드시 업로드 형식에 맞지 않는 이름 변경!
+  function cleanFileName(fileName: string) {
+    return fileName.replace(/[^a-zA-Z0-9.]/g, '_');
+  }
+
+  // supabase storage에 등록한 이미지 업로드
+  const uploadFile = async (file: File) => {
+    const cleanName = cleanFileName(file.name);
+    const filePath = `uploads/${classData?.class_id}_${cleanName}`;
+    const { data, error } = await supabase.storage.from('commentsImages').upload(filePath, file);
+    if (error) {
+      return null;
+    } else {
+      const url = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/commentsImages/${data?.path}`;
+      return url;
+    }
+  };
+  const handleCommentImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
   const { mutate, error, status } = useMutation({
     mutationKey: ['createDetailComment'],
-    mutationFn: () => createDetailComment(classData?.class_id, star, userId, content),
+    mutationFn: () => createDetailComment(classData?.class_id, star, userId, content, commentImage),
     retry: 1,
     onSuccess: () => {
       queryClient.invalidateQueries();
@@ -41,14 +60,17 @@ const CreateComments = ({ classData }: { classData: ListDetailClassInfo | null }
       setStar(0);
     }
   });
+
+  //onchange Content
   const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(event.target.value);
   };
-
+  //onchange star
   const handleStarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setStar(parseInt(event.target.value));
   };
 
+  //후기 등록
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!content.trim() || content.trim().length < 10) {
@@ -80,33 +102,56 @@ const CreateComments = ({ classData }: { classData: ListDetailClassInfo | null }
       {classData?.reserve?.some((reserve) => reserve.user_id === `${loginUserId}`) ? (
         <div className="w-[1000px] bg-disable-color rounded-xl shadow-2xl border-solid p-4">
           <form onSubmit={handleSubmit}>
-            <div className="rating rating-sm flex justify-end items-center">
-              {[1, 2, 3, 4, 5].map((num) => (
-                <input
-                  key={num}
-                  type="radio"
-                  name="rating"
-                  className="mask mask-star-2 mb-1 bg-[#6C5FF7]"
-                  value={num}
-                  onChange={handleStarChange}
-                  checked={star === num}
-                />
-              ))}
+            <div>
+              <label
+                htmlFor="image-upload"
+                className="border border-[#6C5FF7] bg-[#E3E1FC] text-black text-sm p-1 rounded-full hover:bg-[#CAC6FC] hover:border-[#6C5FF7] cursor-pointer"
+              >
+                사진추가
+              </label>
+              <input
+                id="image-upload"
+                type="file"
+                accept="image/*"
+                onChange={handleCommentImageChange}
+                style={{ display: 'none' }}
+              />
+              <Image
+                src={commentImage[0].preview || noImage}
+                alt="uploaded"
+                fill
+                className="h-full w-full object-cover rounded-[20px] border"
+              />
             </div>
-            <textarea
-              minLength={10}
-              maxLength={100}
-              className="w-full h-24 p-2 border rounded-md"
-              placeholder="후기을 입력해주세요.(10자 이상)"
-              value={content}
-              onChange={handleContentChange}
-            ></textarea>
-            <button
-              type="submit"
-              className="mt-2 bg-[#6C5FF7] hover:bg-button-hover-color text-white font-bold py-2 px-4 rounded"
-            >
-              후기 등록
-            </button>
+            <div className="w-[600px] flex flex-col justify-center items-start">
+              <div className="rating min-w-full rating-sm flex justify-end items-center">
+                {[1, 2, 3, 4, 5].map((num) => (
+                  <input
+                    key={num}
+                    type="radio"
+                    name="rating"
+                    className="mask mask-star-2 mb-1 bg-[#6C5FF7]"
+                    value={num}
+                    onChange={handleStarChange}
+                    checked={star === num}
+                  />
+                ))}
+              </div>
+              <textarea
+                minLength={10}
+                maxLength={100}
+                className="w-full h-24 p-2 border rounded-md"
+                placeholder="후기을 입력해주세요.(10자 이상)"
+                value={content}
+                onChange={handleContentChange}
+              ></textarea>
+              <button
+                type="submit"
+                className="mt-2 bg-[#6C5FF7] hover:bg-button-hover-color text-white font-bold py-2 px-4 rounded"
+              >
+                후기 등록
+              </button>
+            </div>
           </form>
         </div>
       ) : null}
