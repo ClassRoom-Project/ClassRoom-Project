215809c98f118d5337b4c0e53cf07c268498f12c
Jiwon <tj1tj2tj3@naver.com>
2024-04-18 21:50:38 +0900

    style: 마진 수정중 #175

--- src/app/(clrm)/reserve/success/[reservationId]/page.tsx
+++ src/app/(clrm)/reserve/success/[reservationId]/page.tsx
@@ -91,14 +91,14 @@ const ReservationCompletePage = ({ params }: { params: { reservationId: string }
       {!isLoading && !isError ? (
         <>
           <div className="w-full min-h-100vh-header-default  box-border bg-light-purple  flex justify-center mobile:justify-center mobile:py-2 items-center flex-col text-gray-700">
-            <div className=" mobile:w-11/12 lg:w-1/2 xl:min-w-1/2 sm:w-11/12 md:w-4/5  md:min-w-[500px] lg:min-w-[600px]  2xl:w-[48%]   justify-center min-h-[600px] bg-white shadow rounded-md items-center flex flex-col px-12 mobile:px-6 pt-12 pb-8">
+            <div className=" mobile:w-11/12 lg:w-1/2 xl:min-w-1/2 sm:w-11/12 md:w-4/5  md:min-w-[500px] lg:min-w-[600px] 2xl:w-[48%] lg:bg-slate-400 xl:bg-slate-700 2xl:bg-orange-400   justify-center min-h-[600px] bg-white shadow rounded-md items-center flex flex-col px-12 mobile:px-6 pt-12 pb-8">
               <FiCheckCircle color="#38c557" className="mb-6 mobile:mb-4" size={70} />
               <h1 className="text-2xl font-bold sm:text-xl  text-center md:text-xl xl:text-2xl lg:text-2xl mobile:text-base">
                 클래스 예약이 정상적으로 처리되었습니다.
               </h1>
               <div className="divider mx-0 sm:my-4 mobile:my-2"></div>
               <div className="flex flex-col items-center w-full justify-center">
-                <div className="flex flex-col bg-base-200 mobile:text-sm sm:w-4/5 md:w-4/5 xl:w-3/5 rounded-lg   py-4 items-center mb-5 gap-2">
+                <div className="flex flex-col bg-base-200 mobile:text-sm sm:w-4/5 md:w-4/5 lg:bg-black lg:w-8/12 rounded-lg py-4 items-center mb-5 gap-2">
                   <p className="text-xl font-bold text-center mx-4 mobile:text-base sm:text-lg">
                     {reservationDetails?.class.title}
                   </p>
@@ -108,14 +108,16 @@ const ReservationCompletePage = ({ params }: { params: { reservationId: string }
                   </div>
                 </div>
                 {/* mobile:ml-4 md:ml-16 lg:ml-20 */}
-                <div className="flex flex-col gap-4 font-bold w-10/12 sm:w-2/3 md:w-2/3 truncate xl:w-2/5 lg:w-1/2 lg:min-w-[290px] 2xl:max-w-[500px]">
+                <div className="flex flex-col gap-4 font-bold w-10/12 sm:w-2/3 md:w-2/3 truncate xl:w-[55%] lg:w-[55%] lg:min-w-[290px] 2xl:max-w-[500px]">
                   {reserveInfoLabels.map(({ icon, title, description }) => (
                     <div key={title} className="flex gap-2 w-full">
                       <div className="flex flex-row gap-2 justify-center items-center">
                         <p className=" text-right">{icon}</p>
                         <p className="mobile:hidden sm:flex flex-shrink-0 mr-2">{title}</p>
                       </div>
-                      <p className={`text-left text-gray-600 font-normal   mobile:text-sm sm:font-normal truncate`}>
+                      <p
+                        className={`text-left text-gray-600 font-normal   mobile:text-sm md:text-base sm:font-normal truncate`}
+                      >
                         {description}
                       </p>
                     </div>
