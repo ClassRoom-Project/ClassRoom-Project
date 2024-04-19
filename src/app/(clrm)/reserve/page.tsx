5cd9b70ebd5c978fa8db8f248e892c1cfb580975
Jiwon <tj1tj2tj3@naver.com>
2024-04-19 11:35:43 +0900

    style: sm이하 width 수정 #175

--- src/app/(clrm)/reserve/page.tsx
+++ src/app/(clrm)/reserve/page.tsx
@@ -22,12 +22,12 @@ export default async function ReservePage({ searchParams }: { searchParams: { cl
 
       {classInfo ? (
         <div className="min-h-100vh-header-reserve m-0 box-border py-6 lg:min-w-fit md:mx-10 lg:mx-16 lg:px-8 bg-light-purple  flex justify-center items-center flex-col text-gray-600 lg:flex-row ">
-          <div className="flex w-1/3 min-w-[350px] sm:w-2/3 lg:w-1/3 lg:min-w-[400px] box-border flex-col h-[780px]  justify-between">
+          <div className="flex w-11/12 min-w-[350px] sm:w-2/3 lg:w-1/3 lg:min-w-[400px] box-border flex-col h-[780px]  justify-between">
             <ClassInfo classInfo={classInfo} />
             <ReserveUserInfo />
           </div>
           <div className="lg:divider-horizontal" />
-          <div className=" md:mt-4 sm:mt-4 mt-4 lg:mt-0 py-4 sm:px-9 min-w-[350px] sm:w-2/3 lg:w-1/3 lg:min-w-[400px] w-1/3 h-[780px] justify-between bg-white rounded-md  flex flex-col shadow px-7">
+          <div className=" md:mt-4 sm:mt-4 mt-4 lg:mt-0 py-4 sm:px-9 min-w-[350px] sm:w-2/3 lg:w-1/3 lg:min-w-[400px] w-11/12 h-[780px] justify-between bg-white rounded-md  flex flex-col shadow px-7">
             <ReservationScheduler classInfo={classInfo} />
             <SetQuantityAndPay classInfo={classInfo} />
           </div>
