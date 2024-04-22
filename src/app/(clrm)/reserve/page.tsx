643a52895c9c6608d85827901fde8aa9a6ad4e3c
Jiwon <tj1tj2tj3@naver.com>
2024-04-22 18:41:19 +0900

    style: 예약 페이지 css 수정#204

--- src/app/(clrm)/reserve/page.tsx
+++ src/app/(clrm)/reserve/page.tsx
@@ -11,8 +11,8 @@ export default async function ReservePage({ searchParams }: { searchParams: { cl
   const classInfo = await fetchReserveClassInfo(classId);
 
   return (
-    <div className="min-w-100vw flex flex-col relative">
-      <div className="m-0 p-2  flex items-center bg-white text-text-dark-gray fixed z-30 w-full">
+    <div className="min-w-100vw flex flex-col">
+      <div className="m-0 p-2  flex items-center bg-white text-text-dark-gray">
         <Link href={`/list/detail/${classInfo.classId}`} className="flex justify-center items-center">
           <IoIosArrowBack size={18} />
           상세보기
@@ -20,13 +20,13 @@ export default async function ReservePage({ searchParams }: { searchParams: { cl
       </div>
 
       {classInfo ? (
-        <div className="min-h-100vh-header-reserve pt-16 pb-20  m-0  md:py-6 md:px-4 lg:min-w-fit  lg:flex-row bg-light-purple  flex flex-col justify-center items-center  text-gray-600 ">
+        <div className="min-h-100vh-header-reserve m-0 pt-6 pb-20 md:py-6 md:px-4 lg:min-w-fit  lg:flex-row bg-light-purple  flex flex-col justify-center items-center  text-gray-600 ">
           <div className="flex flex-col justify-between w-11/12 min-w-[350px] h-[780px] sm:w-2/3 lg:w-1/3 lg:min-w-[400px]   ">
             <ClassInfo classInfo={classInfo} />
             <ReserveUserInfo />
           </div>
           <div className="lg:divider-horizontal" />
-          <div className="mt-4 w-11/12 min-w-[350px] h-[780px] py-4 sm:mt-4 sm:w-2/3 sm:px-9 lg:mt-0 lg:w-1/3 lg:min-w-[400px] flex flex-col justify-between bg-white rounded-md shadow px-7">
+          <div className="mt-4 w-11/12 min-w-[350px] h-[780px] py-4 sm:w-2/3 sm:px-9 lg:mt-0 lg:w-1/3 lg:min-w-[400px] flex flex-col justify-between bg-white rounded-md shadow px-7">
             <ReservationScheduler classInfo={classInfo} />
             <SetQuantityAndPay classInfo={classInfo} />
           </div>
