de7f86a2b5c0ea805cfa2f479ceb23889e28e1e8
Jiwon <tj1tj2tj3@naver.com>
2024-04-25 01:55:58 +0900

    feat: 예약 가능 날짜 출력 #218

--- src/components/classDetail/ClassSummary.tsx
+++ src/components/classDetail/ClassSummary.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ClassDetailBtn from './ClassDetailBtn';
-import Image from 'next/image';
 import { ListDetailClassInfo } from '@/types/class';
 import { DetailUserInfoType } from '@/types/user';
-import { RiHashtag, RiUserLocationLine } from 'react-icons/ri';
+import Image from 'next/image';
+import { FiCalendar, FiUsers } from 'react-icons/fi';
+import { GrLocation } from 'react-icons/gr';
 import { HiOutlineCube } from 'react-icons/hi2';
 import { LuClock } from 'react-icons/lu';
-import { GrLocation } from 'react-icons/gr';
 import { PiCurrencyKrw } from 'react-icons/pi';
-import { FiUsers } from 'react-icons/fi';
-import { FiCalendar, FiCheckCircle, FiUserPlus, FiWatch } from 'react-icons/fi';
+import { RiHashtag, RiUserLocationLine } from 'react-icons/ri';
+import ClassDetailBtn from './ClassDetailBtn';
+import defaultProfileImageSrc from '../../assets/images/profile-image.png';
 
 const ClassSummary = ({
   classData,
@@ -18,7 +17,6 @@ const ClassSummary = ({
   classData: ListDetailClassInfo | null;
   userData: DetailUserInfoType | null;
 }) => {
-  const defaultProfileImageSrc = '/기본프로필사진.png';
   const hashtagString = classData?.hashtag.map((tag) => {
     return (
       <div key={tag} className="flex items-center text-gray-400">
@@ -28,9 +26,10 @@ const ClassSummary = ({
     );
   });
 
-  const classDaysElements = classData?.date.map((item) => {
-    const day = item.day.slice(5); // '2024-01-01' 형태의 문자열에서 원하는 부분을 추출
-    return <div key={day}>{day}</div>; // 바로 반환
+  // 예약 가능 날짜 배열 생성
+  const classDaysElements = classData?.date.map((dateInfo) => {
+    const day = dateInfo.day.slice(5);
+    return <div key={dateInfo.date_id}>{day}</div>;
   });
 
   const classInfoLabels = [
@@ -39,15 +38,21 @@ const ClassSummary = ({
       title: `클래스 유형`,
       description: `${classData?.class_type}`
     },
-    {
-      icon: <HiOutlineCube className="text-main-color" />,
-      title: '난이도',
-      description: classData?.difficulty
-    },
+
     {
       icon: <LuClock className="text-main-color" />,
       title: '소요 시간',
       description: `${classData?.total_time}시간`
+    },
+
+    {
+      icon: <FiUsers className="text-main-color" />,
+      title: '정원',
+      description: (
+        <div>
+          최소 {classData?.min_people}명 ~ 최대 {classData?.quantity}명
+        </div>
+      )
     },
     {
       icon: <GrLocation className="text-main-color" />,
@@ -59,15 +64,7 @@ const ClassSummary = ({
       title: '예약 가능 날짜',
       description: classDaysElements
     },
-    {
-      icon: <FiUsers className="text-main-color" />,
-      title: '정원',
-      description: (
-        <div>
-          최소 {classData?.min_people}명 ~ 최대 {classData?.quantity}명
-        </div>
-      )
-    },
+
     {
       icon: <PiCurrencyKrw className="text-main-color" />,
       title: '수강 금액',
@@ -91,7 +88,7 @@ const ClassSummary = ({
       <div className="flex flex-col gap-4 text-text-dark-gray">
         <div className="text-xl font-bold">{classData?.title}</div>
         <div>
-          <div className="mb-2">카테고리 : {classData?.category}</div>
+          <div className="mb-0.5">카테고리 : {classData?.category}</div>
           <div className="flex gap-2">{hashtagString}</div>
         </div>
         <div className="flex flex-col gap-5 text-lg">
