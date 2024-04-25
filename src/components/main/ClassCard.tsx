c6d0d867461b185a476107df5cf1ec3002044a5d
ocean <twyeo9006@gmail.com>
2024-04-25 22:29:40 +0900

    style : 카드 제목과 태그 간격 수정

--- src/components/main/ClassCard.tsx
+++ src/components/main/ClassCard.tsx
@@ -43,7 +43,7 @@ const ClassCard = ({ classInfos }: { classInfos: ClassAllType }) => {
               </p>
             </div>
           )}
-          <p className="h-16 overflow-hidden py-1 text-xs font-semibold md:text-base">{classInfos.title}</p>
+          <p className="h-9 overflow-hidden py-1 text-xs font-semibold md:h-14 md:text-base">{classInfos.title}</p>
           <p className="h-5 overflow-hidden py-1 text-xs text-dark-purple-color md:h-7 md:text-sm">{hashtagString}</p>
         </div>
       </Link>
