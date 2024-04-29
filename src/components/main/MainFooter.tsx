a2726c9bbc25a6809d39ede6aabb66ed897679ea
ocean <twyeo9006@gmail.com>
2024-04-29 17:37:14 +0900

    style : main footer css 수정 및 반응형 적용

--- src/components/main/MainFooter.tsx
+++ src/components/main/MainFooter.tsx
@@ -4,11 +4,11 @@ import Logo from '@/assets/images/Group 1791.png';
 import Link from 'next/link';
 const MainFooter = () => {
   return (
-    <div className="h-60 w-full bg-background-color">
+    <div className="mt-20 h-full w-full border-t-[1px] border-solid border-gray-400 md:py-4">
       <div className="relative flex h-full items-center justify-center p-5">
-        <div className="relative h-24 w-24">
+        <div className="relative hidden md:block md:h-20 md:w-20">
           <Image
-            sizes="96px"
+            sizes="80px"
             placeholder="empty"
             fill={true}
             src={Logo}
@@ -16,17 +16,17 @@ const MainFooter = () => {
             style={{ objectFit: 'contain' }}
           />
         </div>
-        <div className="flex flex-grow flex-col gap-3 pl-12 pr-3">
+        <div className="flex flex-grow flex-col gap-1 pr-3 md:pl-12">
           <div className="flex items-center justify-between text-xs md:text-sm lg:text-base">
             <p>버그 버스터즈</p>
-            <p className="text-text-dark-gray">ⓒ Bug Busters</p>
+            <p className="text-xs text-text-dark-gray">ⓒ Bug Busters</p>
           </div>
           <div className="text-xs md:text-sm lg:text-base">
-            <Link href={'/bugBusters'} className="text-text-dark-gray hover:text-white">
+            <Link href={'/bugBusters'} className="text-text-dark-gray hover:text-main-color">
               팀 소개
             </Link>
           </div>
-          <div className="flex flex-col gap-2 text-xs md:text-sm lg:text-base">
+          <div className="flex flex-col text-xs md:text-sm">
             <p className="text-text-dark-gray">
               클룸은 원데이 클래스 웹사이트입니다. 이 웹사이트를 통해 더 나은 경험과 성장을 경험하시길 바랍니다.
             </p>
